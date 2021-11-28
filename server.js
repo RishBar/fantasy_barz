67cb6a2d0f4080a7145583206a9c2002ee9b522b
Rishav Baral <rishavbaral@Rishavs-MacBook-Air.local>
2021-11-27 16:07:55 -0800

    require path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express  = require("express");
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config()
+const path = require('path'); 
 const YahooFantasy = require('yahoo-fantasy');
 
 
