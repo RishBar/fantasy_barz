2178cae133dd55e4dbafb160e1a87a1425005345
Rishav Baral <rishavbaral@Rishavs-MacBook-Air.local>
2021-11-29 02:25:17 -0800

    added an alert on the front end landing page so I could try and get the query string out from the url so i can send alerts on the backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, Fragment } from "react";
+import {useLocation} from "react-router-dom";
 
 function App() {
 
-
+  let location = useLocation();
+  let [alerts, setAlerts] =  useState(null) 
+  useEffect(() => {
+    setAlerts(location.pathname)
+  }, [location])
 
   return (
     <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <p>
-        Edit <code>src/App.js</code> and save to reload.
+        {alerts}
       </p>
-      <a
-        className="App-link"
-        href="https://fantasybarz-production.up.railway.app/auth/yahoo"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Yahoo verification
-      </a>
-    </header>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://fantasybarz-production.up.railway.app/auth/yahoo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Yahoo verification
+        </a>
+      </header>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -3,45 +3,57 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config()
 const path = require('path'); 
+var url = require('url');
 const YahooFantasy = require('yahoo-fantasy');
 const redis = require('redis');
 
-var redisPort = process.env.REDISPORT || 6379;
-var redisHost = process.env.REDISHOST || '127.0.0.1';
-var redisAuth = process.env.REDISPASSWORD || null;
+// var redisPort = process.env.REDISPORT || 6379;
+// var redisHost = process.env.REDISHOST || '127.0.0.1';
+// var redisAuth = process.env.REDISPASSWORD || null;
+// var redisURL = url.parse(process.env.REDIS_URL);
 
-(async () => {
-  const client = redis.createClient(redisPort, redisHost, {
-    auth_pass: redisAuth
-  });
-  client.on('error', (err) => console.log('Redis Client Error', err));
-  await client.connect();
-  console.log(client)
-})();
+// process.env.NODE_ENV? 
+// (async () => {
+//   console.log("RIGHTTTTTTTT", process.env.REDIS_URL);
+  
+//   var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+//   client.auth(redisURL.auth.split(":")[1]);
+//   client.on('error', (err) => console.log('Redis Client Error', err));
+//   await client.connect();
+// })() :
+// (async () => {
+//   console.log("NOT RIGHTTTTTTTT");
+  
+//   const client = redis.createClient(redisPort, redisHost);
+//   client.on('error', (err) => console.log('Redis Client Error', err));
+//   await client.connect();
+//   console.log(client)
+// })()
+
 
 
 const app = express(); 
 const port = process.env.PORT || 3001;
 const redirectURI = process.env.REDIRECTURI || "http://localhost:3001/auth/yahoo/callback"
 
-app.tokenCallback = tokenCallback = function ({ access_token, refresh_token }) {
-    return new Promise((resolve, reject) => {
-      // client is redis client
-      client.set("accessToken", access_token, (err, res) => {
-        // could probably handle this with a multi... 
-        // and you know... handle the errors...
-        // good thing this is only an example!
-        client.set("accessToken", access_token, (err, res) => {
-          return resolve();
-        })
-      })
-    });
-  };
+// app.tokenCallback = tokenCallback = function ({ access_token, refresh_token }) {
+//     return new Promise((resolve, reject) => {
+//       // client is redis client
+//       client.set("accessToken", access_token, (err, res) => {
+//         // could probably handle this with a multi... 
+//         // and you know... handle the errors...
+//         // good thing this is only an example!
+//         client.set("accessToken", access_token, (err, res) => {
+//           return resolve();
+//         })
+//       })
+//     });
+//   };
 
 app.yf = new YahooFantasy(
     process.env.YaAPP_KEY,
     process.env.YaAPP_SECRET,
-    tokenCallback,
+    null,
     redirectURI
 );
 
@@ -75,20 +87,61 @@ app.get(
   }
 );
 
+app.get(
+  "/games",
+  (req, res) => {
+    try {
+      app.yf.user.games((uselessParam, gamesResponse) => {
+        if (typeof(gamesResponse) == "Object") {
+          console.log("GOODRESPONSEGOODRESPONSEGOODRESPONSE", typeof(gamesResponse), gamesResponse);
+          res.send(gamesResponse) 
+        } else {
+          console.log("GOODRESPONSEGOODRESPONSEGOODRESPONSE", typeof(gamesResponse), gamesResponse);
+          res.send("There was a problem getting games resource, check if access token is still set")
+        }        
+      });
+    } catch (e) {
+      console.error("ERROR MESSAGE", e);
+    } 
+  }
+);
+// app.get(
+//   "/scores-for-week",
+//   (req, res) => {
+//     try {
+//       const scoreboard = await app.yf.league.scoreboard(
+//         league_key,
+//         week // optional
+//       );
+//     } catch (e) {
+//       // handle error
+//     }
+//   }
+// )
+
+
   
 app.get("/auth/yahoo/callback", (req, res) => {
   console.log("callback route been hit");
+  
   app.yf.authCallback(req, (err) => {
     if (err) {
       return res.redirect("/error");
+    } else if (app.yf.yahooUserToken) {
+      console.log("APPAPPAPAPPAPPAP", app.yf);
+      console.log("TOKENENEENENENENENE",app.yf.yahooUserToken);
+      var string = encodeURIComponent('successfully verified');
+      return res.redirect('/?verified=' + string);
+    } else {
+      console.log("APPAPPAPAPPAPPAP", app.yf);
+      console.log("TOKENENEENENENENENE",app.yf.yahooUserToken);
+      var string = encodeURIComponent('Problem verifying, please try again');
+      return res.redirect('/?verified=' + string);
     }
-    return res.redirect("/");
   });
-  client.get('framework', function(err, reply) {
-    console.log("AYYYYYYYY FAM", reply);
-  });
-  console.log("APPAPPAPAPPAPPAP", app.yf);
-  console.log("USERUSERUSERUSERUSERUSER",app.yf.user);
+  // client.get('framework', function(err, reply) {
+  //   console.log("AYYYYYYYY FAM", reply);
+  // });
 });
 
 app.get("/error", (req, res) => {
